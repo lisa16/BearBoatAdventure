0cfd0632b86a00386dd8b93a9859f200c75ed656
jennylia <jennylian100@gmail.com>
2014-10-04 15:45:24 -0700

    adding 2 scripts for the bears and their opponents

---
+++ Assets/Demo/Scripts/Game/MoveBearBreathOpp.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using BellaProject;
+using Bindings;
+
+public class MoveBearBreath : MonoBehaviour
+{
+
+		//private Rigidbody2D;
+		// Use this for initialization
+		void Start ()
+		{
+				Manager.messenger.Subscribe (BellaMessages.GoodBreath, OnMessage);
+				Manager.messenger.Subscribe (BellaMessages.WeakBreath, OnMessage);
+				Manager.messenger.Subscribe (BellaMessages.StrongBreath, OnMessage);
+				Manager.messenger.Subscribe (BellaMessages.ReadyForInput, OnMessage);
+				Manager.messenger.Subscribe (BellaMessages.BreakTimeStarted, OnMessage);
+				Manager.messenger.Subscribe (BellaMessages.BreakTimeMinReached, OnMessage);
+		}
+
+		void OnDestroy ()
+		{
+				Manager.messenger.Unsubscribe (BellaMessages.GoodBreath, OnMessage);
+				Manager.messenger.Unsubscribe (BellaMessages.WeakBreath, OnMessage);
+				Manager.messenger.Unsubscribe (BellaMessages.StrongBreath, OnMessage);
+				Manager.messenger.Unsubscribe (BellaMessages.ReadyForInput, OnMessage);
+				Manager.messenger.Unsubscribe (BellaMessages.BreakTimeStarted, OnMessage);
+				Manager.messenger.Unsubscribe (BellaMessages.BreakTimeMinReached, OnMessage);
+		}
+		// Update is called once per frame
+		void Update ()
+		{
+
+
+	
+		}
+
+		void OnMessage (Object sender, string msgID, float num1 = 0f, float num2 = 0f, float num3 = 0f, float num4 = 0f)
+		{
+				Debug.Log ("started");
+				if (msgID == BellaMessages.WeakBreath) {
+						Debug.Log ("some weak force");
+						rigidbody2D.velocity = (new Vector2 (0, 0));
+						rigidbody2D.velocity = (new Vector2 (0, 10));
+				}
+				if (msgID == BellaMessages.GoodBreath) {
+						Debug.Log ("some weak force");
+						rigidbody2D.velocity = (new Vector2 (0, 0));
+						rigidbody2D.velocity = (new Vector2 (0, 50));
+				}
+				if (msgID == BellaMessages.StrongBreath) {
+						Debug.Log ("some weak force");
+						rigidbody2D.velocity = (new Vector2 (0, 0));
+						rigidbody2D.velocity = (new Vector2 (0, 20));
+				}
+		}
+}
