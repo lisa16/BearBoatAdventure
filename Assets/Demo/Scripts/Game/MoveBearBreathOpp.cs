78c4461e07255f61afa2ac05b86905b7b33f19eb
jennylia <jennylian100@gmail.com>
2014-10-04 16:45:58 -0700

    ready to merge some changes from Eric

--- Assets/Demo/Scripts/Game/MoveBearBreathOpp.cs
+++ Assets/Demo/Scripts/Game/MoveBearBreathOpp.cs
@@ -8,6 +8,13 @@ public class MoveBearBreathOpp : MonoBehaviour
 
 		//private Rigidbody2D;
 		// Use this for initialization
+
+		
+		public static int v_offset = Random.Range (1, 25);
+		public int multi_weak = -1;
+		public int multi_strong = -1;
+		public int multi_good = -5;
+
 		void Start ()
 		{
 				Manager.messenger.Subscribe (BellaMessages.GoodBreath, OnMessage);
@@ -30,28 +37,37 @@ public class MoveBearBreathOpp : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
-
+				
 
 	
 		}
 
 		void OnMessage (Object sender, string msgID, float num1 = 0f, float num2 = 0f, float num3 = 0f, float num4 = 0f)
 		{
+				int moveGood = multi_good * v_offset;
+				int moveStrong = multi_strong * v_offset;
+				int moveWeak = multi_weak * v_offset;
+
 				Debug.Log ("started");
 				if (msgID == BellaMessages.WeakBreath) {
 						Debug.Log ("some weak force");
-						rigidbody2D.velocity = (new Vector2 (0, 0));
-						rigidbody2D.velocity = (new Vector2 (0, 10));
+						resetVelocity ();
+						rigidbody2D.velocity = (new Vector2 (0, multi_weak));
 				}
 				if (msgID == BellaMessages.GoodBreath) {
 						Debug.Log ("some weak force");
-						rigidbody2D.velocity = (new Vector2 (0, 0));
-						rigidbody2D.velocity = (new Vector2 (0, 50));
+						resetVelocity ();
+						rigidbody2D.velocity = (new Vector2 (0, moveGood));
 				}
 				if (msgID == BellaMessages.StrongBreath) {
 						Debug.Log ("some weak force");
-						rigidbody2D.velocity = (new Vector2 (0, 0));
-						rigidbody2D.velocity = (new Vector2 (0, 20));
+						resetVelocity ();
+						rigidbody2D.velocity = (new Vector2 (0, moveStrong));
 				}
+		}
+
+		void resetVelocity ()
+		{
+				rigidbody2D.velocity = (new Vector2 (0, 0));
 		}
 }
